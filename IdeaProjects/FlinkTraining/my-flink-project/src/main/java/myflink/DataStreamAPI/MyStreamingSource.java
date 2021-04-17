9133a6bbd336206eb1ac61e250340b0a6daedb5a
Hsiaopan <hsiaopan@outlook.com>
2021-04-18 00:38:53 +0800

    DataStreamAPI-Map,FlatMap,Filter

--- IdeaProjects/FlinkTraining/my-flink-project/src/main/java/myflink/DataStreamAPI/MyStreamingSource.java
+++ IdeaProjects/FlinkTraining/my-flink-project/src/main/java/myflink/DataStreamAPI/MyStreamingSource.java
@@ -1,25 +1,27 @@
 package myflink.DataStreamAPI;
 
-import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.streaming.api.datastream.DataStream;
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
-
-import myflink.DataStreamAPI.MyStreamingSource.Item;
+import org.apache.flink.util.Collector;
 
 import java.util.Random;
 
 public class MyStreamingSource implements SourceFunction<MyStreamingSource.Item> {
     private boolean isRuning = true;
+
     /**
-     *  重写run方法产生源源不断的数据发送源
+     * 重写run方法产生源源不断的数据发送源
      */
     @Override
     public void run(SourceContext<Item> ctx) throws Exception {
         while (isRuning) {
-            Item item =generateItem();
+            Item item = generateItem();
             ctx.collect(item);
 
             // 每秒产生一条数据
@@ -40,11 +42,11 @@ public class MyStreamingSource implements SourceFunction<MyStreamingSource.Item>
         item.setName("name" + i);
         item.setId(i);
 
-        return  item;
+        return item;
     }
 
 
-    class Item{
+    class Item {
         private String name;
         private Integer id;
 
@@ -86,13 +88,39 @@ class StreamingDemo {
         itemDataStreamSource = env.addSource(new MyStreamingSource()).setParallelism(1);
 
         // map
-        SingleOutputStreamOperator<Object> mapItems;
-        mapItems = itemDataStreamSource.map((MapFunction<MyStreamingSource.Item, Object>) Item::getName);
+        SingleOutputStreamOperator<String> mapItems;
+        mapItems = itemDataStreamSource.map(new MyMapFunction());
 
-        // 打印结果
-        mapItems.print().setParallelism(1);
+        // flatMap
+        SingleOutputStreamOperator<Object> flatMapItems;
+        flatMapItems = itemDataStreamSource
+                .flatMap(new FlatMapFunction<MyStreamingSource.Item, Object>() {
+                    @Override
+                    public void flatMap(MyStreamingSource.Item item, Collector<Object> collector) throws Exception {
+                        String name = item.getName();
+                        collector.collect(name);
+                    }
+                })
+        ;
+
+        // filter
+        SingleOutputStreamOperator<MyStreamingSource.Item> filterItems;
+        filterItems = itemDataStreamSource.filter((FilterFunction<MyStreamingSource.Item>) item -> item.getId() % 2 == 0);
+
+        /// 打印结果
+        /// mapItems.print().setParallelism(1);
+        /// flatMapItems.print();
+        filterItems.print();
         String jobName = "user defined streaming source";
         env.execute(jobName);
 
+    }
+
+    static class MyMapFunction extends RichMapFunction<MyStreamingSource.Item, String> {
+
+        @Override
+        public String map(MyStreamingSource.Item item) throws Exception {
+            return item.getName();
+        }
     }
 }
