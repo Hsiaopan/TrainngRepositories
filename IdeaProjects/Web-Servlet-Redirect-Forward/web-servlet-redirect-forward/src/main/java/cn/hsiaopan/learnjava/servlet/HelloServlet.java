dc2bc00d4fb06280f85ddcdf8d9b3ce6ff079aff
Hsiaopan <hsiaopan@outlook.com>
2021-05-04 02:58:33 +0800

    Web-Servlet-Redirect-Forward

---
+++ IdeaProjects/Web-Servlet-Redirect-Forward/web-servlet-redirect-forward/src/main/java/cn/hsiaopan/learnjava/servlet/HelloServlet.java
@@ -0,0 +1,25 @@
+package cn.hsiaopan.learnjava.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        String name = req.getParameter("name");
+        if (name == null) {
+            name = "world";
+        }
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("<h1>Hello, " + name + "!</h1>");
+        printWriter.flush();
+    }
+}
