e78d082120a9aefd11889b5bbec85579929b073d
Hsiaopan <hsiaopan@outlook.com>
2021-05-03 18:14:42 +0800

    Web-Servlet-Hello. dynamic name

--- IdeaProjects/Web-Servlet-Hello/HelloServlet/src/main/java/cn/hsiaopan/learnjava/HelloServlet.java
+++ IdeaProjects/Web-Servlet-Hello/HelloServlet/src/main/java/cn/hsiaopan/learnjava/HelloServlet.java
@@ -6,20 +6,24 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/")
 public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException{
+            throws ServletException, IOException {
         // 设置响应类型
         response.setContentType("text/html");
         // 获取输出流
         PrintWriter printWriter = response.getWriter();
+
+        String name = request.getParameter("name");
+        String word = "world";
+
+        if (name != null && !name.isBlank()) word = name;
         // 写入响应
-        printWriter.write("<h1>Hello, world!</h1>");
+        printWriter.write("<h1>Hello, " + word + "!</h1>");
         // flush强制退出
         printWriter.flush();
     }
